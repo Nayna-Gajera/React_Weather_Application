8faffe3643a4e36452db9d3ffddec85a8e31fb2b
Nayna Gajera <nayna.gajera21@gmail.com>
2021-04-19 23:35:27 -0700

    Updates

---
+++ precache-manifest.b01cf12887c39fb567974ad28f0d9938.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5452dbe1ec7018ff27fd50243578450",
+    "url": "/React_Weather_Application/index.html"
+  },
+  {
+    "revision": "4178c293247e09736645",
+    "url": "/React_Weather_Application/static/css/2.7987f354.chunk.css"
+  },
+  {
+    "revision": "7140abefbcca2c8d659d",
+    "url": "/React_Weather_Application/static/css/main.f0d7509e.chunk.css"
+  },
+  {
+    "revision": "4178c293247e09736645",
+    "url": "/React_Weather_Application/static/js/2.f464af3b.chunk.js"
+  },
+  {
+    "revision": "7140abefbcca2c8d659d",
+    "url": "/React_Weather_Application/static/js/main.4f7a97e6.chunk.js"
+  },
+  {
+    "revision": "835d6bdae7979b453cd5",
+    "url": "/React_Weather_Application/static/js/runtime~main.5c6f22db.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/React_Weather_Application/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React_Weather_Application/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React_Weather_Application/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React_Weather_Application/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React_Weather_Application/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React_Weather_Application/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
